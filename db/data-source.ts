a4be90b44785143c4008eb7425a59a781131a29d
Hau. Le Cong <leconghau095@gmail.com>
2024-05-01 20:30:27 +0700

    update typeorm migration

--- db/data-source.ts
+++ db/data-source.ts
@@ -6,6 +6,15 @@ import { Song } from 'src/songs/song.entity'
 import { User } from 'src/users/user.entity'
 import { DataSource, DataSourceOptions } from 'typeorm'
 
+// Load ENV variable
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const path = require('path')
+// const envPath = path.resolve(__dirname, `.env.${process.env.NODE_ENV}`)
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// require('dotenv').config({ path: envPath })
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
@@ -24,17 +33,21 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   }
 }
 
-// export const dataSourceOptions: DataSourceOptions = {
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT),
-//   username: process.env.USERNAME,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   entities: ['dist/**/*.entity.js'], //1
-//   synchronize: false, // 2
-//   migrations: ['dist/db/migrations/*.js'] // 3
-// }
+console.log(process.env.NODE_ENV)
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_PASSWORD)
 
-// const dataSource = new DataSource(dataSourceOptions) //4
-// export default dataSource
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  entities: ['dist/**/*.entity.js'], //1
+  synchronize: false, // 2
+  migrations: ['dist/db/migrations/*.js'] // 3
+}
+
+const dataSource = new DataSource(dataSourceOptions) //4
+export default dataSource
